6773d763a9761f786ff489c0b4cc16f7946bb3af
Chandan Yeshwanth <chandan.yeshwanth@gmail.com>
2015-06-30 16:17:42 +0200

    refactor stddev.py main into functions
    separated main function into several smaller functions to group tweets,
    count tweets, find IDF, file I/O

--- detection/stddev.py
+++ detection/stddev.py
@@ -8,9 +8,21 @@
 # find keywords
 
 import mysql.connector
+
 import datetime
+
 import math
 import numpy
+
+import string
+import nltk
+
+def get_stopwords(fname):
+	with open(fname, 'r') as f:
+		return f.readlines()
+
+STOPWORDS = get_stopwords("stopwords.txt")
+
 
 def init_db():
 	cnx = mysql.connector.connect(user='root', password='internship',
@@ -21,8 +33,10 @@ def init_db():
 
 	return cnx, cursor
 
+def close_db(cnx):
+	cnx.close()	
+
 def get_data_by_date(cursor, start_date, end_date):
-	# query = "select TIME(creation_date), GROUP_CONCAT(content SEPARATOR ' ') from tweet where creation_date between '{0}' and '{1}' limit 10"
 	query = "select creation_date, content from tweet_en where creation_date between '{0}' and '{1}'"
 	query = query.format(start_date, end_date)
 
@@ -33,16 +47,7 @@ def get_data_by_date(cursor, start_date, end_date):
 def get_idf(total, count):
 	return math.log(float(total)/(count + 1))	
 
-def main():
-	cnx, cursor = init_db()
-
-	start_date = raw_input("Enter start date, time (yyyy-mm-dd hh:MM): ")
-	end_date = raw_input("Enter finish date, time (yyyy-mm-dd hh:MM): ") 
-
-	print "Getting data"
-	cursor = get_data_by_date(cursor, start_date, end_date)
-
-	# group tweets by time (minute)
+def group_tweets_by_time(cursor):
 	grouped_tweets = {}
 
 	for (date, tweet) in cursor:
@@ -54,80 +59,89 @@ def main():
 		else:
 			grouped_tweets[time] = [tweet]
 
+	return grouped_tweets
+
+# count occurrences of words per minute and find IDF
+def get_word_IDFseries(grouped_tweets, tweets_per_minute_dict):
+	print "Finding IDFs"
+	
+	word_IDFseries = {}
+
+	for time, tweets in grouped_tweets.iteritems():
+		word_count_dict = {}
+
+		tweet_text = " ".join(tweets)
+		
+		# remove apostrophe
+		# replace other punctuation with a whitespace
+		# then tokenize on whitespace
+		words = tweet_text.replace("'", "").translate(None, string.punctuation).split()
+		words = [w for w in words if w not in STOPWORDS]
+
+		for word in words:
+			if word in word_count_dict:
+				word_count_dict[word] += 1
+			else:
+				word_count_dict[word]  = 1
+
+		# find IDF for each word in this minute
+		for word in word_count_dict:
+			idf = get_idf(tweets_per_minute_dict[time], word_count_dict[word])
+
+			if word not in word_IDFseries:
+				# create a list
+				word_IDFseries[word] = [idf]
+			else:	
+				# append to list
+				word_IDFseries[word].append(idf)
+
+	return word_IDFseries
+
+def write_to_file(word_stdev):
+	print "Writing to file"
+
+	with open("stdev.out", "w") as f:
+		for w, c in word_stdev:
+			s = w + ", " + str(c) + "\n"
+			s = s.encode("utf8")
+			f.write(s)
+
+def get_word_stdev_from_IDFseries(word_IDFseries):
+	word_stdev = {}
+
+	print "Finding stdevs"
+	for word in word_IDFseries:
+		# data series is stored as array in word_stdev
+		word_stdev[word] = numpy.std(word_IDFseries[word])
+
+	return word_stdev
+		
+def main():
+	cnx, cursor = init_db()
+
+	start_date = raw_input("Enter start date, time (yyyy-mm-dd hh:MM): ")
+	end_date = raw_input("Enter finish date, time (yyyy-mm-dd hh:MM): ") 
+
+	print "Getting data"
+	cursor = get_data_by_date(cursor, start_date, end_date)
+
+	# group tweets by time (minute)
+	grouped_tweets = group_tweets_by_time(cursor)
+	
 	tweets_per_minute_dict = {
 		time : len(tweets) for (time, tweets) in grouped_tweets.iteritems()
 	}
 
-	# a dict to store 
-	# 	{ time of day (HH:mm) : 
-	# 		{
-	# 			word1 : IDF in this minute
-	#			word2 : ...
-	# 		}
-	#	  time2 : ...
-	# 	}
-	# time_wordIDF_dict = {}
+	word_IDFseries = get_word_IDFseries(grouped_tweets, tweets_per_minute_dict)		
 
-	word_stdev = {}
+	print "Unique words: ", len(word_IDFseries)
 
-	print "Finding IDFs"
-	# count occurrences of words per minute and find IDF
-	for time, tweets in grouped_tweets.iteritems():
-		wordIDF_dict = {}
-
-		words = " ".join(tweets).lower().split()
-
-		for word in words:
-			if word in wordIDF_dict:
-				wordIDF_dict[word] += 1
-			else:
-				wordIDF_dict[word]  = 1
-
-			# initialize with None, fill the std dev of IDF later 
-			if word not in word_stdev:
-				word_stdev[word] = []
-
-		# find IDF for each word in this minute
-		for word in wordIDF_dict:
-			# wordIDF_dict[word] = get_idf(tweets_per_minute_dict[time], wordIDF_dict[word])
-			# store data series in word_stdev
-			word_stdev[word].append(
-					get_idf(tweets_per_minute_dict[time], wordIDF_dict[word])
-				)
-
-		# add this dict to main dict
-		# time_wordIDF_dict[time] = wordIDF_dict
-
-	print "Unique words: ", len(word_stdev)
-
-	print "Finding stdevs"
-	for word in word_stdev:
-		# data_series = []
-
-		# for time in time_wordIDF_dict:
-		# 	# if the word occurred in this time use IDF, else IDF with 0 count
-		# 	data_series.append(
-		# 		time_wordIDF_dict[time][word] if word in time_wordIDF_dict[time]
-		# 		else get_idf(tweets_per_minute_dict[time], 0)
-		# 	)
-
-		# word_stdev[word] = numpy.std(data_series)
-		# data series is stored as array in word_stdev
-		word_stdev[word] = numpy.std(word_stdev[word])
-
+	# dict for word - std dev of word IDF in this time period
+	word_stdev = get_word_stdev_from_IDFseries(word_IDFseries)
 	sorted_stdev = sorted(word_stdev.iteritems(), key=lambda x:x[1], reverse=True)
 
-	print "Writing to file"
-
-	f = open("stdev.out", "w")
-	for w, c in sorted_stdev[:100]:
-		print w, c
-		s = w + ", " + str(c) + "\n"
-		s = s.encode("utf8")
-		f.write(s)
-	f.close()
-	
-	cnx.close()
+	write_to_file(sorted_stdev)
+	close_db(cnx)	
 
 if __name__ == '__main__':
 	main()
